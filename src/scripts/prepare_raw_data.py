7c4482d799e5f99162b88656958c2c6b5bb614e4
derRiesenOtter <104461717+derRiesenOtter@users.noreply.github.com>
2025-05-12 17:25:59 +0200

    Modifications on scripts

--- src/scripts/prepare_raw_data.py
+++ src/scripts/prepare_raw_data.py
@@ -1,11 +1,30 @@
+import os
 import pickle
+import sys
 
 import pandas as pd
 
-data = pd.read_csv("./data/raw_data/llps_data_ppmclab.tsv", delimiter="\t")
 
-data.loc[~data["Datasets"].isin(["NP", "ND"]), "PS"] = 0
-data.loc[data["Datasets"].isin(["NP", "ND"]), "PS"] = 1
+def main():
+    df = pd.read_csv("./data/raw_data/llps_data_ppmclab.tsv", delimiter="\t")
 
-with open("./data/intermediate_data/llps_data_ppmclab.pkl", "wb") as f:
-    pickle.dump(data, f)
+    df.loc[~df["Datasets"].isin(["NP", "ND"]), "PS"] = 0
+    df.loc[df["Datasets"].isin(["NP", "ND"]), "PS"] = 1
+
+    filtered_df = df[~df["Full.seq"].str.contains("X|U", na=False)]
+
+    print(f"Size of the whole data set: {len(df)} proteins.")
+    print(f"Size of the filtered data set: {len(filtered_df)} proteins.")
+    print("Positive Samples:")
+    print(len(df.loc[~df["Datasets"].isin(["NP", "ND"]), "PS"]))
+    print("Negative Samples:")
+    print(len(df.loc[df["Datasets"].isin(["NP", "ND"]), "PS"]))
+
+    with open("./data/intermediate_data/llps_data_ppmclab.pkl", "wb") as f:
+        pickle.dump(filtered_df, f)
+
+
+if __name__ == "__main__":
+    filename = os.path.basename(__file__)
+    sys.stdout = open(f"./results/stdout/{filename}.txt", "wt")
+    main()
---
+++ src/scripts/run_block_decomposition.py
@@ -0,0 +1,71 @@
+import pickle
+from functools import partial
+from multiprocessing import Pool, freeze_support
+
+from src.modules import Words
+from src.modules.block_decomposition_modifier import get_block_seq
+from src.modules.Words.Mappings import (
+    APNAMapping,
+    IDRMapping,
+    MM5Mapping,
+    PIPIFMapping,
+    PIPIGMapping,
+    RGMapping,
+)
+
+
+def create_block_list(sequence: str, balance_threshold: int, mapping):
+    w = Words.Word.MappableWord([_ for _ in sequence], mapping)
+    block_list = w.find_balanced_subwords(balance_threshold)
+    return block_list
+
+
+def main():
+    with open("./data/intermediate_data/llps_data_ppmclab.pkl", "rb") as f:
+        data = pickle.load(f)
+    mappings = [
+        APNAMapping,
+        RGMapping,
+        PIPIFMapping,
+        PIPIGMapping,
+        MM5Mapping,
+        IDRMapping,
+    ]
+    with Pool(8) as p:
+        for mapping in mappings:
+            balance_threshold = 4
+            func = partial(
+                create_block_list, balance_threshold=balance_threshold, mapping=mapping
+            )
+            data[f"{mapping.__name__}_{balance_threshold}"] = p.map(
+                func, data["Full.seq"]
+            )
+
+            data[f"{mapping.__name__}_{balance_threshold}_vec"] = [
+                get_block_seq(seq, blocks, mapping.Forward)
+                for seq, blocks in zip(
+                    data["Full.seq"], data[f"{mapping.__name__}_{balance_threshold}"]
+                )
+            ]
+
+            balance_threshold = 3
+            func = partial(
+                create_block_list, balance_threshold=balance_threshold, mapping=mapping
+            )
+            data[f"{mapping.__name__}_{balance_threshold}"] = p.map(
+                func, data["Full.seq"]
+            )
+            data[f"{mapping.__name__}_{balance_threshold}_vec"] = [
+                get_block_seq(seq, blocks, mapping.Forward)
+                for seq, blocks in zip(
+                    data["Full.seq"], data[f"{mapping.__name__}_{balance_threshold}"]
+                )
+            ]
+
+    with open("./data/intermediate_data/llps_data_ppmclab_bd.pkl", "bw") as f:
+        pickle.dump(data, f)
+
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
